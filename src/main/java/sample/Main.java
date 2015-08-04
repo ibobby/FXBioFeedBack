e7c18bf721863538949a8d4c0c6dedc5a2d3dd9b
bobby <ibobby0386@gmail.com>
2015-08-04 23:05:42 +0300

    add test label

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,8 +1,11 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.LabelBuilder;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,9 +19,26 @@ public class Main extends Application {
         Group root = new Group();
         primaryStage.setTitle("bio");
         primaryStage.setScene(new Scene(root, windowWidth, windowHeight));
+
+        root.getChildren().add(createLabel());
+
         primaryStage.show();
     }
 
+    private Label createLabel() {
+        Label label = LabelBuilder.create().
+                text("some").
+                prefWidth(100).
+                prefHeight(50).
+                alignment(Pos.CENTER).
+                layoutX(500).
+                layoutY(550).
+                style("-fx-background-color: orange;").
+                build();
+
+        return label;
+
+    }
 
     public static void main(String[] args) {
         launch(args);
